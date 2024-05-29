96a6e0f7f15fab3a4fb05b5832fd113882b4492a
tanujveera <tanuj.veera@gmail.com>
2024-05-29 10:04:02 +0530

    Body Component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { LANDING_PAGE } from "../utils/constants";
 
 const Body = () => {
   return (
-    <div>
-      <div>
-        <span>Human</span> 
+    <div className="flex justify-evenly">
+      <div className="grid grid-cols-1">
+        <span>Human</span>
         <span>stories & ideas</span>
         <span>A place to read, write, and deepen your understanding</span>
         <button>Get Started</button>
+      </div>
+      <div>
+        <img className="w-2/4" src={LANDING_PAGE} alt="Landing Page" />
       </div>
     </div>
   );
