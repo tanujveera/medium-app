8148839abe52a1d698c7273d45013b94d6379d8e
tanujveera <tanuj.veera@gmail.com>
2024-06-15 18:55:35 +0530

    About Header

--- src/components/About/AboutHeader.jsx
+++ src/components/About/AboutHeader.jsx
@@ -15,12 +15,16 @@ const AboutHeader = () => {
       <div className="flex justify-between p-6 bg-black">
         <div>
           <Link to="/">
-          <img className="w-44" src={MEDIUM_LOGO_WHITE} alt="Medium Logo" />
+            <img className="w-44" src={MEDIUM_LOGO_WHITE} alt="Medium Logo" />
           </Link>
         </div>
         <div className="flex">
-          <div className="mr-6 text-gray-200 rounded-full border border-white px-4 py-2 hover:text-white cursor-pointer" >Sign in</div>
-          <div className=" text-black rounded-full bg-slate-200 px-4 py-2 hover:bg-white cursor-pointer">Sign up</div>
+          <Link to="/signin">
+            <div className="mr-6 text-gray-200 rounded-full border border-white px-4 py-2 hover:text-white cursor-pointer" >Sign in</div>
+          </Link>
+          <Link to="/signup">
+            <div className=" text-black rounded-full bg-slate-200 px-4 py-2 hover:bg-white cursor-pointer">Sign up</div>
+          </Link>
         </div>
       </div>
       <hr className="border border-gray-500 opacity-50"></hr>
