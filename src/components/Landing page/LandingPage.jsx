d580cc26d22b5540135ecd5731631b0fde22f065
tanujveera <tanuj.veera@gmail.com>
2024-06-24 08:59:37 +0530

    Trying to connect with a backend

--- src/components/Landing page/LandingPage.jsx
+++ src/components/Landing page/LandingPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Body from "./Body";
 import Features from "./Features";
 import Footer from "./Footer";
 import LandingHeader from "./LandingHeader";
 import { aboutFeatures } from "../../utils/constants";
 import PageTitle from "../../utils/PageTitle";
+import {api, pageData} from "../../utils/URL_Constants.js"
 
 const LandingPage = () => {
+  const getData = async()=>{
+    const apiData = await fetch(api+pageData)
+    const {data} = await apiData.json();
+    
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div>
       <PageTitle title="Medium – Where good ideas find you." />
--- src/utils/URL_Constants.js
+++ src/utils/URL_Constants.js
@@ -1,1 +1,7 @@
 export const firebase_twitter = "https://medium-app-10b31.firebaseapp.com/__/auth/handler";
+export const api = "http://localhost:5000/app/v1"
+
+
+
+// Endpoints
+export const pageData = "/pageInfo"
