d231cf9d06d6eaa0e4a51a6bbe18ea184fc9b84c
tanujveera <tanuj.veera@gmail.com>
2024-07-18 09:09:37 +0530

    Added loading

--- front-end/src/utils/protectedRoute.js
+++ front-end/src/utils/protectedRoute.js
@@ -1,15 +1,11 @@
 // ProtectedRoute.js
-import React, { useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { checkAuthStatus } from "../redux/authSlice";
 import Error from "./Error"
 import useCheckAuthStatus from "./hooks/useCheckAuthStatus";
 
 const ProtectedRoute = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
 
   useCheckAuthStatus();
@@ -23,9 +19,9 @@ const ProtectedRoute = () => {
   //   },2000)
   // }, [isAuthenticated, loading, navigate]);
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return isAuthenticated ? <Outlet /> : <Error/>;
 };
